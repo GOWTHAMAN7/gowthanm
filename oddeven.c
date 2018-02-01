6e38a6b01bafc5d1138181e39f675641b66336c6
GOWTHAMAN7 <35653111+GOWTHAMAN7@users.noreply.github.com>
2018-02-01 16:15:53 +0530

    Create oddeven.c

---
+++ oddeven.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+    int number;
+    printf("enter the number");
+    scanf("%d",&number);
+    if(n%==2)
+    printf("even");
+    else
+    printf("odd");
+
+    return 0;
+}
